4e2429e70ab680e3d095bbcec3f6c2c284a2bf8c
Igor Sidorov <2019igorsidorov@gmail.com>
2022-12-11 19:35:09 +0300

    Task#25_Task#27_Lesson4

---
+++ lesson_4/Task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ Math.Pow
+
+int numA = 2;
+int numB = 4;
+
+void countDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    Console.Write(result);
+}
+countDegree(numA, numB);
---
+++ lesson_4/Task_27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// Строки использовать нельзя
+
+int num = 9012;
+int i = 0;
+int countIndex(int a)
+{
+    while (a > 0)
+    {
+        a /= 10;
+        i++;
+    }
+    return i;
+};
+countIndex(num);
+
+void countSumNumber(int i, int b)
+{
+    int result = 0;
+    for (int j = 0; j <= i; j++)
+    {
+        result += b % 10;
+        b /= 10;
+    }
+    Console.WriteLine(result);
+
+};
+countSumNumber(i, num);
+
+
+
