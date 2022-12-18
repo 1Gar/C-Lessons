3b346fb246bf2388d7bd21405f8944c2127fac75
Igor Sidorov <2019igorsidorov@gmail.com>
2022-12-18 23:54:51 +0300

    Finished Task#47

--- lesson_7/Task_47/Lesson7.cs
+++ lesson_7/Task_47/Lesson7.cs
@@ -3,6 +3,10 @@ public class Lesson_7
 {
     public static void Task_47()
     {
-
+        int column = InformationFromUser("Введите количество столбцов");
+        int lines = InformationFromUser("Введите количество строк");
+        double[,] array = CreateArray(lines, column);
+        FillArray(array);
+        PrintArray(array);
     }
 }
--- lesson_7/Task_47/MyLibrary.cs
+++ lesson_7/Task_47/MyLibrary.cs
@@ -1,4 +1,47 @@
 public class MyLibrary
 {
+    //1. Функция вывода текста и получения данных от пользователя
+
+    public static int InformationFromUser(string text)
+    {
+        Console.WriteLine(text);
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+
+    //2. Функция создания двухмерного массива исходя их данных пользователя
+
+    public static double[,] CreateArray(int lines, int column)
+    {
+        return new double[lines, column];
+    }
+
+    //3. Функция заполнения массива вещественными числами
+
+    public static void FillArray(double[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                numbers[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            }
+        }
+    }
+    //4. Функция вывода массива
+
+    public static void PrintArray(double[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            Console.Write("");
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                Console.Write(numbers[i, j] + " ");
+            }
+            Console.Write("");
+            Console.WriteLine("");
+        }
+    }
 
 }
