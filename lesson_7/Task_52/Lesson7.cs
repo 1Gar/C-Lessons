b9b706990b015a5c123ba3ae0cb6ef72d045083f
Igor Sidorov <2019igorsidorov@gmail.com>
2022-12-19 00:53:15 +0300

    Finished Task#52

--- lesson_7/Task_52/Lesson7.cs
+++ lesson_7/Task_52/Lesson7.cs
@@ -3,6 +3,11 @@ public class Lesson_7
 {
     public static void Task_52()
     {
-
+        int column = InformationFromUser("Введите количество столбцов");
+        int lines = InformationFromUser("Введите количество строк");
+        int[,] array = CreateArray(lines, column);
+        FillArray(array);
+        PrintArray(array);
+        AverageNumber(array, lines);
     }
 }
--- lesson_7/Task_52/MyLibrary.cs
+++ lesson_7/Task_52/MyLibrary.cs
@@ -1,4 +1,62 @@
 public class MyLibrary
 {
-    
+    //1. Функция вывода текста и получения данных от пользователя
+
+    public static int InformationFromUser(string text)
+    {
+        Console.WriteLine(text);
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+
+    //2. Функция создания двухмерного массива исходя их данных пользователя
+
+    public static int[,] CreateArray(int lines, int column)
+    {
+        return new int[lines, column];
+    }
+
+    //3. Функция заполнения массива вещественными числами
+
+    public static void FillArray(int[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                numbers[i, j] = Convert.ToInt32(new Random().Next(0, 10));
+            }
+        }
+    }
+    //4. Функция вывода массива
+
+    public static void PrintArray(int[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            Console.Write("");
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                Console.Write(numbers[i, j] + " ");
+            }
+            Console.Write("");
+            Console.WriteLine("");
+        }
+    }
+    //5. Нахождение среднего арифметического числа в каждом столбце
+    public static void AverageNumber(int[,] numbers, int lines)
+    {
+
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            double result = 0;
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                result = (result + numbers[i, j]);
+            }
+            result = result / lines;
+            Console.Write(result + " ");
+        }
+
+    }
 }
