bc11420b8ce5ecdcb0209ea924a63a4bc90b8086
Igor Sidorov <2019igorsidorov@gmail.com>
2022-12-18 16:29:15 +0300

    Fixed Task_43 add new text

--- lesson_6/Task_43/Lesson6.cs
+++ lesson_6/Task_43/Lesson6.cs
@@ -4,7 +4,7 @@ public class Lesson_6
     public static void Task_43()
     {
         double b1 = WriteLine("введите значение b1");
-        double k1 = WriteLine("введите число k1");
+        double k1 = WriteLine("введите значение k1");
         double b2 = WriteLine("введите значение b2");
         double k2 = WriteLine("введите значение k2");
         Print(b1, k1, b2, k2);
