9f2a4565568b8b39d8d045daf6b87a2ff1a52c4b
Igor Sidorov <2019igorsidorov@gmail.com>
2022-12-19 23:25:20 +0300

    Finished Task#50

--- lesson_7/Task_50/Lesson7.cs
+++ lesson_7/Task_50/Lesson7.cs
@@ -3,6 +3,12 @@ public class Lesson_7
 {
     public static void Task_50()
     {
+        int lines = InformationFromUser("Введите номер строки");
+        int column = InformationFromUser("Введите номер столбца");
+        int[,] array = CreateArray(10, 10);
+        FillArray(array);
+        PrintArray(array);
+        FindNumber(array, lines, column);
 
     }
 }
--- lesson_7/Task_50/MyLibrary.cs
+++ lesson_7/Task_50/MyLibrary.cs
@@ -1,3 +1,57 @@
-public class MyLibrary{
-    
+public class MyLibrary
+{
+    //1. Функция вывода текста и получения данных от пользователя
+
+    public static int InformationFromUser(string text)
+    {
+        Console.WriteLine(text);
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+
+    }
+    //2. Функция создания двухмерного массива
+
+    public static int[,] CreateArray(int lines, int column)
+    {
+        return new int[lines, column];
+    }
+    //3. Функция заполнения массива числами
+
+    public static void FillArray(int[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                numbers[i, j] = Convert.ToInt32(new Random().Next(0, 10));
+            }
+        }
+    }
+    //4. Функция вывода массива
+    public static void PrintArray(int[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            Console.Write("");
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                Console.Write(numbers[i, j] + " ");
+            }
+            Console.Write("");
+            Console.WriteLine("");
+        }
+    }
+    //5. Нахождение элемента в массиве
+    public static void FindNumber(int[,] numbers, int lines, int column)
+    {
+        if (lines > numbers.GetLength(0) || column > numbers.GetLength(1))
+        {
+            Console.WriteLine("Такого элемента нет");
+        }
+        else
+        {
+            Console.WriteLine($"Значение элемента {lines} строки и {column} столбца равно {numbers[lines - 1, column - 1]}");
+        }
+    }
+
 }
