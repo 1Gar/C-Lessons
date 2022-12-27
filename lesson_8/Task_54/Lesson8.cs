6b5e92842a99c77efeb86fbac12d93e7ceb65760
Igor Sidorov <2019igorsidorov@gmail.com>
2022-12-27 15:04:17 +0300

    Finished Task#54

--- lesson_8/Task_54/Lesson8.cs
+++ lesson_8/Task_54/Lesson8.cs
@@ -3,6 +3,13 @@ public class Lesson_8
 {
     public static void Task_54()
     {
-
+        int column = InformationFromUser("Введите количество столбцов");
+        int lines = InformationFromUser("Введите количество строк");
+        int[,] array = CreateArray(lines, column);
+        FillArray(array);
+        PrintArray(array);
+        OrderArrayLines(array);
+        Console.WriteLine();
+        PrintArray(array);
     }
 }
--- lesson_8/Task_54/MyLibrary.cs
+++ lesson_8/Task_54/MyLibrary.cs
@@ -1,4 +1,68 @@
 public class MyLibrary
 {
+    //1. Функция вывода текста и получения данных от пользователя
+
+    public static int InformationFromUser(string text)
+    {
+        Console.WriteLine(text);
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+
+    //2. Функция создания двухмерного массива исходя их данных пользователя
+
+    public static int[,] CreateArray(int lines, int column)
+    {
+        return new int[lines, column];
+    }
+
+    //3. Функция заполнения массива числами
+
+    public static void FillArray(int[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                numbers[i, j] = Convert.ToInt32(new Random().Next(0, 10));
+            }
+        }
+    }
+    //4. Функция вывода массива
+
+    public static void PrintArray(int[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            Console.Write("");
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                Console.Write(numbers[i, j] + " ");
+            }
+            Console.Write("");
+            Console.WriteLine("");
+        }
+
+    }
+
+    //5. Функция упорядочения элементов двуxмерного массива
+    public static void OrderArrayLines(int[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                for (int k = 0; k < numbers.GetLength(1) - 1; k++)
+                {
+                    if (numbers[i, k] < numbers[i, k + 1])
+                    {
+                        int temp = numbers[i, k + 1];
+                        numbers[i, k + 1] = numbers[i, k];
+                        numbers[i, k] = temp;
+                    }
+                }
+            }
+        }
+    }
 
 }
