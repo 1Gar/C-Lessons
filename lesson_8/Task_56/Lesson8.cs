cfcb5984b15a5c9ad2e5ca0b1b50fcc7afeb3cc9
Igor Sidorov <2019igorsidorov@gmail.com>
2022-12-27 19:05:55 +0300

    Finished Task#56

--- lesson_8/Task_56/Lesson8.cs
+++ lesson_8/Task_56/Lesson8.cs
@@ -3,6 +3,13 @@ public class Lesson_8
 {
     public static void Task_56()
     {
-
+        int column = InformationFromUser("Введите количество столбцов");
+        int lines = InformationFromUser("Введите количество строк");
+        int[,] array = CreateArray(lines, column);
+        FillArray(array);
+        PrintArray(array);
+        Console.WriteLine();
+        int sumLine = SumLine(array, 0);
+        MinSum(array);
     }
 }
--- lesson_8/Task_56/MyLibrary.cs
+++ lesson_8/Task_56/MyLibrary.cs
@@ -1,4 +1,76 @@
 public class MyLibrary
 {
+    //1. Функция вывода текста и получения данных от пользователя
 
+    public static int InformationFromUser(string text)
+    {
+        Console.WriteLine(text);
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+
+    //2. Функция создания двухмерного массива исходя их данных пользователя
+
+    public static int[,] CreateArray(int lines, int column)
+    {
+        return new int[lines, column];
+    }
+
+    //3. Функция заполнения массива числами
+
+    public static void FillArray(int[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                numbers[i, j] = Convert.ToInt32(new Random().Next(0, 10));
+            }
+        }
+    }
+    //4. Функция вывода массива
+
+    public static void PrintArray(int[,] numbers)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            Console.Write("");
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                Console.Write(numbers[i, j] + " ");
+            }
+            Console.Write("");
+            Console.WriteLine("");
+        }
+
+    }
+
+    // 5. Функция нахождения суммы элементов строк 
+    public static int SumLine(int[,] numbers, int i)
+    {
+        int sumLine = numbers[i, 0];
+        for (int j = 1; j < numbers.GetLength(1); j++)
+        {
+            sumLine += numbers[i, j];
+        }
+        return sumLine;
+    }
+
+    //6. Функция нахождения минимальной суммы строк
+    public static void MinSum(int[,] numbers)
+    {
+        int minSumLine = 0;
+        int sumLine = SumLine(numbers, 0);
+        for (int i = 1; i < numbers.GetLength(0); i++)
+        {
+            int tempSumLine = SumLine(numbers, i);
+            if (sumLine > tempSumLine)
+            {
+                sumLine = tempSumLine;
+                minSumLine = i;
+            }
+        }
+        Console.WriteLine($"Cтрокa с наименьшей суммой элементов - {minSumLine + 1}");
+
+    }
 }
