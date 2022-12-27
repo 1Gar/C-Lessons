adbc5b72af6e80a2d2559800a277221fe2687ddb
Igor Sidorov <2019igorsidorov@gmail.com>
2022-12-27 23:15:10 +0300

    Finished Task#68

--- lesson_9/Task_68/Lesson9.cs
+++ lesson_9/Task_68/Lesson9.cs
@@ -3,6 +3,11 @@ public class Lesson_9
 {
     public static void Task_68()
     {
+        int intM = InformationFromUser("Введите число M");
+        int intN = InformationFromUser("Введите число N");
+        int result = AkkermanFunction(intM, intN);
+        Console.WriteLine();
+        Console.Write($"A(m,n) =  {result}");
 
     }
 }
--- lesson_9/Task_68/MyLibrary.cs
+++ lesson_9/Task_68/MyLibrary.cs
@@ -1,4 +1,18 @@
 public class MyLibrary
 {
+    //1. Функция вывода текста и получения данных от пользователя
 
+    public static int InformationFromUser(string text)
+    {
+        Console.WriteLine(text);
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+    //2. Функция Аккермана
+    public static int AkkermanFunction(int m, int n)
+    {
+        if (m == 0) return n + 1;
+        else if (n == 0) return AkkermanFunction(m - 1, 1);
+        else return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+    }
 }
