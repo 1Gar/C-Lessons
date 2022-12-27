909f5feff81a791fc415b0ec9575a98df48a13a1
Igor Sidorov <2019igorsidorov@gmail.com>
2022-12-27 21:01:07 +0300

    Finished Task#64

--- lesson_9/Task_64/Lesson9.cs
+++ lesson_9/Task_64/Lesson9.cs
@@ -3,6 +3,7 @@ public class Lesson_9
 {
     public static void Task_64()
     {
-
+        int number = InformationFromUser("Введите натуральное число, которое больше 1 ");
+        FindNaturalInt(number);
     }
 }
--- lesson_9/Task_64/MyLibrary.cs
+++ lesson_9/Task_64/MyLibrary.cs
@@ -1,4 +1,23 @@
 public class MyLibrary
 {
+    //1. Функция вывода текста и получения данных от пользователя
+
+    public static int InformationFromUser(string text)
+    {
+        Console.WriteLine(text);
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+    //2. Функция вывода натуральных чисел
+    public static void FindNaturalInt(int number)
+    {
+        int n = number;
+        if (number == 0) return;
+        {
+            Console.Write(number + " ");
+            FindNaturalInt(number - 1);
+        }
+    }
+
 
 }
